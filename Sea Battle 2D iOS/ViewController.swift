03f21d3d6a0e6ea9b7f1a574da4b233d07a361b3
Vlad Nechyporenko <vlladdd28@gmail.com>
2019-10-01 13:05:07 +0300

    Initial Commit

---
+++ Sea Battle 2D iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sea Battle 2D iOS
+//
+//  Created by Vlad Nechiporenko on 10/1/19.
+//  Copyright © 2019 Vlad Nechyporenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
